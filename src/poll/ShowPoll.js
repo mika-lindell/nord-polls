3f41c92015aa49370f9966efc52ec06a249117d4
Mika Lindell <mika.lindell82@gmail.com>
2017-03-03 16:52:11 +0200

    Poll results are now displayed

--- src/poll/ShowPoll.js
+++ src/poll/ShowPoll.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react'
+import {Link} from 'react-router'
+import {find} from 'lodash'
 import {get} from '../helpers.js'
 
 class ShowPoll extends Component{
   state = {
     poll: {},
+    totalVotes: 0,
   }
   componentWillMount() {
     get(`poll/${this.props.params.id}/results`).then((response)=> {
+      console.log(response.data.votes)
       this.setState({
         poll: Object.assign({}, response.data),
       })
@@ -16,9 +20,26 @@ class ShowPoll extends Component{
     if(!this.state.poll.id) return null
     return(
       <div>
-        <h1></h1>
+        <dl>
+          <dt>{this.state.poll.title}</dt>
+          {
+            this.state.poll.choices.map((choice, i)=> {
+              return (
+                <dd key={i}>
+                {choice.label}: {this.getVotes(choice)}
+                </dd>
+              )
+            })
+          }
+        </dl>
+        <Link to={`/poll/${this.state.poll.id}/vote`}>Cast a new vote</Link>
       </div>
     )
+  }
+  getVotes(choice) {
+    const record = find(this.state.poll.votes, {choice_id: choice.id})
+    if(!record) return 0
+    return record.votes
   }
 }
 
