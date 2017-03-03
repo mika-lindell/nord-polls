b926a70a167f699121faa18431b27a2fbb50b863
Mika Lindell <mika.lindell82@gmail.com>
2017-03-03 11:46:10 +0200

    Load poll from api

--- src/poll/poll.js
+++ src/poll/poll.js
@@ -1,7 +1,19 @@
 import React, {Component} from 'react'
+import {get} from '../helpers.js'
 
 class Poll extends Component{
 
+  state = {
+    poll: {},
+  }
+
+  componentWillMount() {
+    get(`poll/${this.props.params.id}`).then((response)=> {
+      this.setState({
+        poll: Object.assign({}, response),
+      })
+    })
+  }
   render() {
     return(
       <div>
@@ -9,6 +21,10 @@ class Poll extends Component{
       </div>
     )
   }
+}
+
+Poll.propTypes = {
+  params: React.PropTypes.object,
 }
 
 export default Poll
