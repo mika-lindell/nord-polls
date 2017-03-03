b30052fcec69f52f8734cbf0ddbb99450cc83d8b
Mika Lindell <mika.lindell82@gmail.com>
2017-03-03 10:15:45 +0200

    Component for viewing single poll +  routes

--- src/root/routes.js
+++ src/root/routes.js
@@ -2,9 +2,13 @@
 
 import React from 'react'
 import {Route, IndexRoute} from 'react-router'
+import CreatePoll from '../poll/CreatePoll'
 import Poll from '../poll/Poll'
 
 export default
   <Route path="/">
-    <IndexRoute component={Poll}/>
+    <IndexRoute component={CreatePoll}/>
+    <Route path="/poll/create" component={CreatePoll}/>
+    <Route path="/poll/:id" component={Poll}/>
+    <Route path="*" component={CreatePoll}/>
   </Route>
