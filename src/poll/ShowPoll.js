b1c725cc6d16195bde2d61a4a3449a1187ec12b7
Mika Lindell <mika.lindell82@gmail.com>
2017-03-03 15:35:24 +0200

    Load requested poll in results-view

--- src/poll/ShowPoll.js
+++ src/poll/ShowPoll.js
@@ -1,12 +1,29 @@
 import React, {Component} from 'react'
+import {get} from '../helpers.js'
 
 class ShowPoll extends Component{
-
+  state = {
+    poll: {},
+  }
+  componentWillMount() {
+    get(`poll/${this.props.params.id}/results`).then((response)=> {
+      this.setState({
+        poll: Object.assign({}, response.data),
+      })
+    })
+  }
   render() {
+    if(!this.state.poll.id) return null
     return(
-      <div>ShowPoll</div>
+      <div>
+        <h1></h1>
+      </div>
     )
   }
+}
+
+ShowPoll.propTypes = {
+  params: React.PropTypes.object,
 }
 
 export default ShowPoll
